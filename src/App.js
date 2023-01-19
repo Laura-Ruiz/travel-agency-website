821b821efc9ebdf08fc2dd383a5c1157d50cb369
Laura-Ruiz <laaura.26@gmail.com>
2023-01-19 12:58:03 +0100

    travel-agency

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Recommend from "./components/Recommend";
+import ScrollToTop from "./components/ScrollToTop";
+import Services from "./components/Services";
+import Testimonials from "./components/Testimonials";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ScrollToTop/>
+      <Navbar/>
+      <Hero/>
+      <Services/>
+      <Recommend/>
+      <Testimonials/>
+      <Footer/>
     </div>
   );
 }
